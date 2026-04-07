60136cda54c2e595f3a2685cdae880ef9d5f9467
Laura <est.laurad.nietom@unimilitar.edu.co>
2026-04-07 09:57:28 -0500

    Ejecucion sin errores, se corrigen errores de ejecucion, el usuario puede ingresar el tamaño y los datos (en cualquier orden) para que el programa los organice

--- ordenamiento/src/main/java/Ordenamiento.java
+++ ordenamiento/src/main/java/Ordenamiento.java
@@ -1,30 +1,32 @@
-import java.util.*;
+package com.mycompany.ordenamiento;
+
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Ordenamiento {
+
     public static void main(String[] args) {
-        
-         Scanner sc = new Scanner(System.in);
-        
-         System.out.println("De que tamaño sera el arreglo?");
-         int n = sc.nextInt();
-         
-         int arr[] = new int[n];
-         
-         //Solicitar datos
-          for (int i = 0; i < n; i++) {
-            System.out.print("Ingrese el Numero"+ i + ": ");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese el tamaño de su arreglo?: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Solicitar datos
+        for (int i = 0; i < n; i++) {
+            System.out.print("Ingrese el número " + i + ": ");
             arr[i] = sc.nextInt();
         }
-        
-        
-        
+
         ordenar(arr);
-        
-        System.out.println(java.util.Arrays.toString(arr));
+       System.out.println(java.util.Arrays.toString(arr));
     }
-    private static void ordenar(int v[]) {
-        for (int i = 0; i < v.length-1; i++) {
-            for (int j = i+1; j < v.length ; j++) {
+
+    private static void ordenar(int[] v) {
+        for (int i = 0; i < v.length - 1; i++) {
+            for (int j = i + 1; j < v.length; j++) {
                 if (v[i] > v[j]) {
                     int w = v[j];
                     v[j] = v[i];
